f7d4031fee1d1d7dddb97772762c8782f34d27a3
Balaji3810 <63021146+Balaji3810@users.noreply.github.com>
2022-09-20 09:35:38 -0500

    Update rev_k_v.py

--- rev_k_v.py
+++ rev_k_v.py
@@ -8,3 +8,15 @@ rev_map = {v:k for k, v in cars_map.items()}
 for i in cars_map:
     dict2.setdefault(cars_map[i],[]).append(i)
 print(dict2)
+
+s="hello"
+s1 = list(s)
+print(s1)
+s2 = {}
+for i in s1:
+    s2[i] = 1 + s2.get(i, 0)
+print(s2)
+s3 = {}
+for k, v in s2.items():
+    s3[v] =s2.get(v, []) + [k]
+print(s3)
