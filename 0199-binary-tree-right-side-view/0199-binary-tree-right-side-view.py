a79f76d33864762769fdf7a1de2f6f852434ec7d
Balaji3810 <63021146+Balaji3810@users.noreply.github.com>
2022-11-13 16:49:58 -0600

    Update 0199-binary-tree-right-side-view.py
    Detailed code

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -1,11 +1,40 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from typing import List
+import collections
+import json
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def fromlist(values):
+    def create(it):
+        value = next(it)
+        return None if value is None else TreeNode(value)
+        
+    if not values:
+        return None
+    it = iter(values)
+    root = TreeNode(next(it))
+    nextlevel = [root]
+    try:
+        while nextlevel:
+            level = nextlevel
+            nextlevel = []
+            for node in level:
+                if node:
+                    node.left = create(it)
+                    node.right = create(it)
+                    nextlevel += [node.left, node.right]
+        
+    except StopIteration:
+        return root
+    raise ValueError("Invalid list")
+    
 class Solution:
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+    def rightSideView(self, root: TreeNode) -> List[int]:
         res = []
         q = collections.deque([root])
         
@@ -23,3 +52,5 @@ class Solution:
             if rightSide:
                 res.append(rightSide.val)
         return res
+s = Solution()
+print(s.rightSideView(fromlist([1,2,3,None,5,None,4])))
