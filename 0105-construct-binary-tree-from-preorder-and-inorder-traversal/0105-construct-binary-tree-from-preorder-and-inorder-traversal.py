0eb9d6ceb8900b7dfeaaf0f36927bf2bd607f647
Balaji3810 <63021146+Balaji3810@users.noreply.github.com>
2022-11-13 22:48:05 -0600

    Update 0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
    Detailed code

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -1,4 +1,43 @@
 # Definition for a binary tree node.
+from typing import List
+from typing import Optional
+import collections
+import json
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    def __repr__(self) -> str:
+      return '[%s, %r, %r]' % (self.val, self.left, self.right)
+
+
+def fromlist(values):
+    def create(it):
+        value = next(it)
+        return None if value is None else TreeNode(value)
+        
+    if not values:
+        return None
+    it = iter(values)
+    root = TreeNode(next(it))
+    nextlevel = [root]
+    try:
+        while nextlevel:
+            level = nextlevel
+            nextlevel = []
+            for node in level:
+                if node:
+                    node.left = create(it)
+                    node.right = create(it)
+                    nextlevel += [node.left, node.right]
+        
+    except StopIteration:
+        return root
+    raise ValueError("Invalid list")
+    
+# Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
@@ -15,3 +54,6 @@ class Solution:
         root.right = self.buildTree(preorder[mid +1:], inorder[mid +1:])
 
         return root
+      
+s = Solution()
+print(s.buildTree([3,9,20,15,7],[9,3,15,20,7]))
